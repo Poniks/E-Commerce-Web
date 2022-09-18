12463d9bbb587ee2dd4bab9ecec761dc1a074600
Poniks <nadstogakuba@gmail.com>
2022-09-18 23:58:09 +0200

    Dashboard.v0.3

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Header from "../../Header";
 import CarouselImage from './Carousel';
 import shoes from "../../../images/buty1.jpg";
+import shoes2 from "../../../images/buty2.jpg";
+
+const products = [
+    {name: "Białe Najkacze", image: shoes, price: "120zł"},
+    {name: "Czarne Adidosy", image: shoes2, price: "250zł"},
+    {name: "Białe Pumy", image: shoes, price: "99zł"},
+    {name: "Czarne Konfersy", image: shoes2, price: "333zł"},
+]
 
 const Dashboard = () => {
     return (
@@ -12,50 +20,20 @@ const Dashboard = () => {
                 <div className="new-products">
                     <h2>Nowe Produkty</h2>
                     <div className="products">
-                        <div className="product">
-                            <a href="/">
-                                <img src={shoes} alt="shoes" />
-                                <div className="description">
-                                    <h3>Białe najkacze</h3>
-                                    <div className="product-price">
-                                        <span className="price">120zł</span>
+                        {products.map(item => {
+                            return (
+                            <div className="product">
+                                <a href="/">
+                                    <img src={item.image} alt="shoes" />
+                                    <div className="description">
+                                        <h3>{item.name}</h3>
+                                        <div className="product-price">
+                                            <span className="price">{item.price}</span>
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="product">
-                            <a href="/">
-                                <img src={shoes} alt="shoes" />
-                                <div className="description">
-                                    <h3>Białe najkacze</h3>
-                                    <div className="product-price">
-                                        <span className="price">120zł</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="product">
-                            <a href="/">
-                                <img src={shoes} alt="shoes" />
-                                <div className="description">
-                                    <h3>Białe najkacze</h3>
-                                    <div className="product-price">
-                                        <span className="price">120zł</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="product">
-                            <a href="/">
-                                <img src={shoes} alt="shoes" />
-                                <div className="description">
-                                    <h3>Białe najkacze</h3>
-                                    <div className="product-price">
-                                        <span className="price">120zł</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
+                                </a>
+                            </div>
+                        )})}
                     </div>
                 </div>
             </div>
